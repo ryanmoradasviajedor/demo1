274c1a00a81bcf27e26b50ce395cb0877d14b716
Ryan Viajedor <ryanviajedor@Ryans-MacBook-Pro.local>
2019-11-23 01:59:06 +0800

    add yaml file

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    target = os.environ.get('TARGET', 'Angular Cebu!')
+    target = os.environ.get('TARGET', 'Angular Cebu')
     return 'Hello {}!\n'.format(target)
 
 if __name__ == "__main__":
